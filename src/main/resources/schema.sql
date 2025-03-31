31b49b21b8ed0a0180329db6a509852778fc0d0d
saarmoseri <xgrm406@gmail.com>
2025-03-31 11:59:00 +0300

    update sql schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,25 @@
 CREATE TABLE IF NOT EXISTS task (
-                                    description VARCHAR(64) NOT NULL,
+    description VARCHAR(64) NOT NULL,
     completed   VARCHAR(30) NOT NULL);
+
+CREATE TABLE IF NOT EXISTS movies (
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(100) NOT NULL,
+        genre VARCHAR(100) NOT NULL,
+        duration INTEGER NOT NULL,
+        rating NUMERIC(5, 2) NOT NULL,
+        releaseYear INT CHECK (releaseYear BETWEEN 1900 AND 2100));
+
+CREATE TABLE IF NOT EXISTS showtimes (
+        id SERIAL PRIMARY KEY,
+        movieId INTEGER NOT NULL REFERENCES movies(id),
+        price NUMERIC(5, 2) NOT NULL,
+        theater VARCHAR(100) NOT NULL,
+        startTime DATE NOT NULL,
+        endTime DATE NOT NULL);
+
+CREATE TABLE IF NOT EXISTS bookings (
+        bookingId UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+        showtimeId INTEGER NOT NULL REFERENCES showtimes(id),
+        seatNumber INTEGER NOT NULL,
+        userId UUID NOT NULL);
